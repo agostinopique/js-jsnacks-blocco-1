7d08706df7774852095d3d34e8733d6b9a632454
agostinopique <ago.pique@yahoo.it>
2022-04-12 18:57:23 +0200

    Completato JSnack9;

---
+++ jsnack9/js/script.js
@@ -0,0 +1,23 @@
+/*
+JSnack 9
+    Calcola la somma e la media dei primi 10 numeri.
+*/
+
+const submit = document.querySelector('#submit');
+
+const iterations = 10;
+let somma = 0;
+let media = 0;
+
+for(i = 1; i <= iterations; i++) {
+
+    somma += i;
+    console.log(somma);
+
+}
+
+media = somma / iterations;
+
+console.log(media);
+
+submit.innerHTML = `La somma dei numeri é ${somma} e la loro media é ${media}`;
