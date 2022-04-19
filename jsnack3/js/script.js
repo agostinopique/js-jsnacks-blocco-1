f6144054ad5d03020ab0ab57f2a1343f339403ef
agostinopique <ago.pique@yahoo.it>
2022-04-19 12:08:45 +0200

    Aggiunta condizione di verifica in caso di inserimetno di Na

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -8,43 +8,51 @@
 /*
 
     1. Chiedo all'utente di inserire un numero, chiedo per 5 volte.
-    2. Salvo gli input in un array.
-    3. Sommo tutti gli elementi dell'array con un ciclo for e stampo a schermo.
-    4. Sommo tutti gli elementi dell'array con un ciclo while e stampo a schermo.
+    2. Sommo tutti gli elementi salvando il risultato in una variabile con un ciclo for e stampo a schermo.
+    4. Sommo tutti gli elementi salvando il risultato in una variabile con un ciclo while e stampo a schermo.
 
 */
 
 const inputLimit = 5
 
-let userInput;
 
 const submit = document.querySelector('#submit');
 
-let arrSum = 0;
+let sum = 0;
+
 
 for(let i = 0; i < inputLimit; i++) {
+    let userInput;
+    while(isNaN(userInput)){
+            userInput = parseInt(prompt(`Inserisci un numero per ${inputLimit - i} volte!`));
 
-    userInput = parseInt(prompt('Inserisci un numero!'));
-    arrSum += userInput;
-    console.log('somma con for', arrSum);
+        }
+
+    sum += userInput;
+    console.log('somma con for', sum);
 }
 
 
-submit.innerHTML = `La somma dei tuoi con for numeri é ${arrSum}`;
-*/
+submit.innerHTML = `La somma dei tuoi numeri con for é ${sum}`;
+
 
 // ALTERNATIVA CON WHILE
 /*
 let i = 0;
 
 while(i < inputLimit) {
+    let userInput;
 
-    userInput = parseInt(prompt('Inserisci un numero!'));
-    arrSum += userInput;
-    console.log('somma con while', arrSum);
+    // controllo
+    while(isNaN(userInput)){
+        userInput = parseInt(prompt(`Inserisci un numero per ${inputLimit - i} volte!`));
+
+    }
+    sum += userInput;
+    console.log('somma con while', sum);
     i++;
 
 }
 
-submit.innerHTML = `La somma dei tuoi numeri con while é ${arrSum}`;
+submit.innerHTML = `La somma dei tuoi numeri con while é ${sum}`;
 */
